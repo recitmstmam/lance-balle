93a1d54969c96a318771f919c7fbaa1afbc10dcb
Marc-André Mercier <75087183+recitmstmam@users.noreply.github.com>
2021-02-18 18:21:22 -0500

    Ajout vd et vg

--- main.ts
+++ main.ts
@@ -1,4 +1,6 @@
 input.onButtonPressed(Button.A, function () {
+    pins.analogWritePin(AnalogPin.P1, Vd)
+    pins.analogWritePin(AnalogPin.P2, Vg)
     pins.digitalWritePin(DigitalPin.P12, 0)
     pins.digitalWritePin(DigitalPin.P13, 1)
     pins.digitalWritePin(DigitalPin.P14, 1)
@@ -15,4 +17,8 @@ input.onButtonPressed(Button.B, function () {
     basic.pause(500)
     servos.P0.setAngle(24)
 })
+let Vg = 0
+let Vd = 0
 servos.P0.setAngle(24)
+Vd = 1023
+Vg = 1023
